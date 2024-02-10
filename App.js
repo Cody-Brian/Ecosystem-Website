749d647395017d77415075b7bdf705ba77160f46
Ivy Das <121896501+Cody-Brian@users.noreply.github.com>
2024-02-10 14:35:32 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,101 @@
+let sh = "SHARK";
+let jl = "JELLYFISH";
+let oc = "OCTOPUS";
+let wh = "WHALE";
+let bwh = "BLUE WHALE";
+let swh = "SEI WHALE";
+let bowh = "BOWHEAD WHALE";
+let cjl = "CRYSTAL JELLYFISH";
+let fejl = "FRIED EGG JELLYFISH";
+let ajl = "ATOLLA JELLYFISH";
+let hsh = "HAMMER HEAD SHARK";
+let gwsh = "GREAT WHITE SHARK";
+let lsh = "LEOPARD SHARK";
+let zsh = "ZEBRA SHARK";
+let broc = "BLUE RING OCTOPUS";
+let moc = "MIMIC OCTOPUS";
+let coc = "COCONUT OCTOPUS";
+let bloc = "BLANKET OCTOPUS";
+
+function searchFunction(){
+    var input, txtValue;
+    input = document.getElementById('txtInput');
+    txtValue = input.value;
+    
+    if(txtValue == sh){
+        window.location.assign("http://127.0.0.1:5501/Shark.html");
+
+    }
+    else if(txtValue == jl){
+        window.location.assign("http://127.0.0.1:5501/Jellyfish.html");
+        
+    }
+    else if(txtValue == oc){
+        window.location.assign("http://127.0.0.1:5501/Octopus.html");
+        
+    }
+    else if(txtValue == wh){
+        window.location.assign("http://127.0.0.1:5501/Whale.html");
+        
+    }
+    else if(txtValue == wh){
+        window.location.assign("http://127.0.0.1:5501/Whale.html");
+        
+    }
+    else if(txtValue == bwh){
+        window.location.assign("http://127.0.0.1:5501/Blue%20Whale.html");
+        
+    }
+    else if(txtValue == swh){
+        window.location.assign("http://127.0.0.1:5501/Sei%20Whale.html");
+        
+    }
+    else if(txtValue == bowh){
+        window.location.assign("http://127.0.0.1:5501/Bowhead%20Whale.html");
+        
+    }
+    else if(txtValue == cjl){
+        window.location.assign("http://127.0.0.1:5501/Crystal%20jelly.html");
+        
+    }
+    else if(txtValue == fejl){
+        window.location.assign("http://127.0.0.1:5501/Friedegg%20Jelly.html");
+        
+    }
+    else if(txtValue == ajl){
+        window.location.assign("http://127.0.0.1:5501/Atolla%20jelly.html");
+        
+    }
+    else if(txtValue == hsh){
+        window.location.assign("http://127.0.0.1:5501/Hammerhead%20Shark.html");
+        
+    }
+    else if(txtValue == gwsh){
+        window.location.assign("http://127.0.0.1:5501/Great%20white%20Shark.html");
+        
+    }
+    else if(txtValue == lsh){
+        window.location.assign("http://127.0.0.1:5501/Leopard%20Shark.html");
+        
+    }
+    else if(txtValue == zsh){
+        window.location.assign("http://127.0.0.1:5501/Zebra%20Shark.html");
+        
+    }
+    else if(txtValue == broc){
+        window.location.assign("http://127.0.0.1:5501/Blue%20Ring%20Octopus.html");
+        
+    }
+    else if(txtValue == moc){
+        window.location.assign("http://127.0.0.1:5501/Mimic%20Octopus.html");
+        
+    }
+    else if(txtValue == coc){
+        window.location.assign("http://127.0.0.1:5501/Coconut%20Octopus.html");
+        
+    }
+    else if(txtValue == bloc){
+        window.location.assign("http://127.0.0.1:5501/Blanket%20Octopus.html");
+        
+    }
+}
